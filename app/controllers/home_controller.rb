8a3245ddb5c00c8ba3ab9af6b2069d0e64c7eddb
a960401 <a960401sogang@gmail.com>
2017-04-06 09:50:16 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def new
+    end
+    
+    def create
+        @post=Post.new
+        @post.title=params[:post_title]
+        @post.content=params[:post_content]
+        @post.save
+        
+        redirect_to "/index"
+    end
+end
